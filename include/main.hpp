1c57ed7b9e42360010f58b101b345b81f467aa38
Marco Tan <marco.tan.200405@gmail.com>
2020-10-17 09:17:07 -0400

    :tada: Add beginning project files.

---
+++ include/main.hpp
@@ -0,0 +1,13 @@
+//* Mirai 2020 - Team E
+//* App:    TBD
+//* Desc:   TBD
+
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+//* Include C++ Headers
+#include <iostream>
+
+//* Include C Headers
+
+#endif // MAIN_HPP
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//* Mirai 2020 - Team E
+//* App:    TBD
+//* Desc:   TBD
+
+#include "main.hpp"
+
+int main()
+{
+    std::cout << "Test.\n";
+    return 0;
+}
